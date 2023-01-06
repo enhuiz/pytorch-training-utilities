906aef7548cdb4cb5e00b880a5f2d311dc68f2ef
enhuiz <niuzhe.nz@outlook.com>
2023-01-06 14:13:39 +0800

    Remove module bakcward hook, only output computes per axis

--- diagnostic.py
+++ diagnostic.py
@@ -32,14 +32,103 @@ class Diagnostic:
         self._max_pca_dim = max_pca_dim
         self._percentiles = percentiles
 
+    def _accumulate_along_axis(self, name, tensor, axis):
+        tensor = tensor.transpose(axis, -1)
+        while tensor.dim() <= 1:
+            tensor = tensor.unsqueeze(0)
+        tensor = tensor.flatten(0, -2)  # (n d)
+
+        size = tensor.shape[-1]
+
+        a = self._history["abs"][name]
+        if isinstance(a, Tensor) and len(a) != size:
+            # Size mismatch, skip.
+            return
+
+        if size < self._max_pca_dim and len(tensor) > 1:
+            self._history["pca"][name] += torch.pca_lowrank(tensor)[1].cpu()
+
+        self._history["abs"][name] += tensor.abs().sum(0).cpu()
+        self._history["pos"][name] += tensor.clamp_min(0).sum(0).cpu()
+        self._history["val"][name] += tensor.sum(0).cpu()
+        self._history["rms"][name] += tensor.pow(2).sum(0).cpu()
+        self._history["cnt"][name] += len(tensor)
+
+        a = self._history["min"][name]
+        b = tensor.min(0).values.cpu()
+        if not isinstance(a, Tensor):
+            a = torch.full_like(b, float("inf"))
+        self._history["min"][name] = torch.minimum(a, b)
+
+        a = self._history["max"][name]
+        b = tensor.max(0).values.cpu()
+        if not isinstance(a, Tensor):
+            a = torch.full_like(b, -float("inf"))
+        self._history["max"][name] = torch.maximum(a, b)
+        self._history["size"][name] = size
+
+    @torch.no_grad()
+    def _accumulate(self, name, tensor, per_axis=True):
+        if per_axis:
+            for axis in range(tensor.dim()):
+                self._accumulate_along_axis(name + f"/axis_{axis}", tensor, axis=axis)
+        else:
+            self._accumulate_along_axis(name, tensor.flatten()[None], axis=-1)
+
+    @global_leader_only
+    def attach(self):
+        # If not detached, don't attach
+        if len(self._handlers) > 0:
+            return
+
+        _logger.info(f"Attaching diagnostic for module {self._tag}.")
+
+        for name, module in self._module.named_modules():
+
+            def forward_hook(m, i, o, name=name or self._tag):
+                if not isinstance(o, tuple):
+                    o = (o,)
+
+                for i, oi in enumerate(o):
+                    if not isinstance(oi, Tensor):
+                        continue
+
+                    self._accumulate(f"{name}/output/{i}", oi)
+
+            self._handlers.append(module.register_forward_hook(forward_hook))
+
+        for name, param in self._module.named_parameters():
+            if not param.requires_grad:
+                continue
+
+            def hook(grad, name=name, param=param):
+                self._accumulate(f"{name}/param", param)
+                self._accumulate(f"{name}/grad", grad, per_axis=False)
+
+            self._handlers.append(param.register_hook(hook))
+
+    @global_leader_only
+    def detach(self):
+        for handler in self._handlers:
+            handler.remove()
+        self._handlers.clear()
+        self._history.clear()
+
+    @global_leader_only
+    def save(self, detach=True):
+        path = artifacts.get_path(f"diagnostic/{self._tag}", ".csv")
+        self.to_csv(path, index=False)
+        if detach:
+            self.detach()
+
     @staticmethod
     def _get_type(name):
         if "grad" in name:
             return "grad"
-        elif "output" in name:
-            return "output"
         elif "param" in name:
             return "param"
+        elif "output" in name:
+            return "output"
         raise NotImplementedError(name)
 
     @property
@@ -49,6 +138,7 @@ class Diagnostic:
         for col in df.columns:
             if col not in ["min", "max", "size"]:
                 df[col] /= df["cnt"]
+
             if col in ["rms"]:
                 df[col] = df[col].apply(torch.sqrt)
 
@@ -74,10 +164,8 @@ class Diagnostic:
                 )
 
                 if v is not None:
-                    row |= {
-                        f"p{i}": v
-                        for i, v in enumerate(np.percentile(v, self._percentiles))
-                    }
+                    percentiles = np.percentile(v.numpy(), self._percentiles)
+                    row |= {f"p{i}": v for i, v in enumerate(percentiles)}
 
                 rows.append(row)
 
@@ -87,104 +175,6 @@ class Diagnostic:
             df = df.sort_values(["type", "stats", "name"])
 
         return df
-
-    def _accumulate_by_dim(self, name, x, dim):
-        x = x.transpose(dim, -1)
-        while x.dim() <= 1:
-            x = x.unsqueeze(0)
-        x = x.flatten(0, -2)  # (n d)
-
-        size = x.shape[-1]
-
-        a = self._history["abs"][name]
-        if isinstance(a, Tensor) and len(a) != size:
-            # Size mismatch, skip.
-            return
-
-        if size < self._max_pca_dim and len(x) > 1:
-            self._history["pca"][name] += torch.pca_lowrank(x)[1].cpu()
-
-        self._history["abs"][name] += x.abs().sum(0).cpu()
-        self._history["pos"][name] += x.clamp_min(0).sum(0).cpu()
-        self._history["val"][name] += x.sum(0).cpu()
-        self._history["rms"][name] += x.pow(2).sum(0).cpu()
-        self._history["cnt"][name] += len(x)
-
-        a = self._history["min"][name]
-        b = x.min(0).values.cpu()
-        if not isinstance(a, Tensor):
-            a = torch.full_like(b, float("inf"))
-        self._history["min"][name] = torch.minimum(a, b)
-
-        a = self._history["max"][name]
-        b = x.max(0).values.cpu()
-        if not isinstance(a, Tensor):
-            a = torch.full_like(b, -float("inf"))
-        self._history["max"][name] = torch.maximum(a, b)
-        self._history["size"][name] = size
-
-    @torch.no_grad()
-    def _accumulate(self, name, x):
-        for d in range(x.dim()):
-            self._accumulate_by_dim(name + f"/dim_{d}", x, d)
-
-    @global_leader_only
-    def attach(self):
-        # If not detached, don't attach
-        if len(self._handlers) > 0:
-            return
-
-        _logger.info(f"Attaching diagnostic for module {self._tag}.")
-
-        for name, module in self._module.named_modules():
-
-            def forward_hook(m, i, o, name=name or self._tag):
-                if not isinstance(o, tuple):
-                    o = (o,)
-
-                for i, oi in enumerate(o):
-                    if not isinstance(oi, Tensor):
-                        continue
-
-                    self._accumulate(f"{name}/output/{i}", oi)
-
-            self._handlers.append(module.register_forward_hook(forward_hook))
-
-            def backward_hook(m, i, o, name=name or self._tag):
-                if not isinstance(o, tuple):
-                    o = (o,)
-
-                for i, oi in enumerate(o):
-                    if not isinstance(oi, Tensor):
-                        continue
-
-                    self._accumulate(f"{name}/grad/{i}", oi)
-
-            self._handlers.append(module.register_full_backward_hook(backward_hook))
-
-        for name, param in self._module.named_parameters():
-            if not param.requires_grad:
-                continue
-
-            def hook(grad, name=name, param=param):
-                self._accumulate(f"{name}/param", param)
-                self._accumulate(f"{name}/grad", grad)
-
-            self._handlers.append(param.register_hook(hook))
-
-    @global_leader_only
-    def detach(self):
-        for handler in self._handlers:
-            handler.remove()
-        self._handlers.clear()
-        self._history.clear()
-
-    @global_leader_only
-    def save(self, detach=True):
-        path = artifacts.get_path(f"diagnostic/{self._tag}", ".csv")
-        self.to_csv(path, index=False)
-        if detach:
-            self.detach()
 
     @property
     def to_csv(self):
@@ -196,11 +186,23 @@ class Diagnostic:
 
 
 if __name__ == "__main__":
-    model = nn.Linear(10, 10)
+    model = nn.Linear(5, 10)
     diagnostic = Diagnostic(model)
     model.bias.requires_grad_(False)
     diagnostic.attach()
-    model(torch.randn(10)).sum().backward()
-    model(torch.randn(10)).sum().backward()
-    model(torch.randn(10)).sum().backward()
+
+    model(torch.randn(2, 5)).sum().backward()
+
+    df = diagnostic.dataframe
+    df = df[df["type"] == "grad"]
+    df = df[df["stats"] == "rms"]
+
+    print(df)
+    print(
+        {
+            n: p.grad.norm() if p.grad is not None else None
+            for n, p in model.named_parameters()
+        }
+    )
+
     print(diagnostic.to_markdown(index=False, floatfmt=".3g"))
